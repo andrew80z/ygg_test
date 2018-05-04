77be67e7e1eaaca68b4afd40022b4787b87bb39c
andrew80z <36384426+andrew80z@users.noreply.github.com>
2018-05-04 17:17:40 +0200

    upd2

--- ygg.py
+++ ygg.py
@@ -1,6 +1,5 @@
 import requests
-
-
+#
 separator = "*" * 10 
 # fn values
 fn_auth_val = "authenticate"
@@ -12,10 +11,9 @@ game_org = "Demo"
 game_lang = "en"
 bet_amount_curr = 50
 bet_amount_coin = 2.00
-
 # Main address
 base_url = "https://pff.yggdrasilgaming.com/game.web/service"
-
+#
 print "'Win test' run"
 print separator
 print "Prerequisite: Loose and won coutners are equal zero"
@@ -27,8 +25,7 @@ print "Send first GET request and obtain a Wager ID"
 print "Send second GET request using Wager ID"
 print "If won payload was NOT returned, increment loose counter by 1 and repeat"
 print "If won payload was returned, increment win counter by 1, stop test run, print win amount and amount of 'played' rounds"
-
-
+#
 # URL to be used for auth GET request as Anonymous
 auth_url = "%s?fn=%s&org=%s&lang=%s&gameid=%s" % (base_url,fn_auth_val,game_org,game_lang,game_id_val)
 auth_resp = requests.get(auth_url)
@@ -44,8 +41,6 @@ print "Session id is: %s" % session_id
 print "Sending play GET requests"
 
 def test_run():
-	
-
 	#URL to be used for 1st play  GET request
 	play_url = "%s?fn=%s&currency=EUR&gameid=%s&sessid=%s&log=&gameHistorySessionId=seesion&gameHistoryTicketId=ticket&amount=%s&lines=1111111111111111111111111&coin=%s" %(base_url,fn_play_val,game_id_val,session_id,bet_amount_curr,bet_amount_coin)
 	#print "Sending first request\n"
@@ -65,7 +60,6 @@ def test_run():
 win_count = 0
 loose_count = 1 
 while win_count == 0:
-
 	my_test = test_run()
 	if 'data' not in my_test:
 		print "User lost."
@@ -77,6 +71,4 @@ while win_count == 0:
 		print "Test finished\n"
 		print "Won amount is: %s" % fin_resp
 		win_count += 1
-		print "Iterations performed: %s" % loose_count
-	
-	
+		print "Iterations performed: %s" % loose_count	
